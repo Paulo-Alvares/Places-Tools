c2ebd7fe03d9400a19bccc8bf1d823d6d7b64cc5
Paulo-Alvares <paulo.alvares@sptech.school>
2025-04-25 13:58:39 -0300

    test: removing validation user in short_url

--- api/shorten.js
+++ api/shorten.js
@@ -15,15 +15,6 @@ module.exports = async (req, res) => {
 
   if (req.method === "POST") {
     const { url } = req.body;
-    let userId = null;
-
-    // Verifica se há token para associar o link ao usuário
-    const authHeader = req.headers.authorization;
-    if (authHeader) {
-      const token = authHeader.split(" ")[1];
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      userId = decoded.id;
-    }
 
     if (!url) {
       return res.status(400).json({ error: "URL is required" });
@@ -34,8 +25,8 @@ module.exports = async (req, res) => {
 
     try {
       const result = await pool.query(
-        "INSERT INTO links (original_url, short_url, user_id) VALUES ($1, $2, $3) RETURNING *",
-        [url, shortUrl, userId]
+        "INSERT INTO links (original_url, short_url) VALUES ($1, $2) RETURNING *",
+        [url, shortUrl]
       );
       res.status(201).json(result.rows[0]);
     } catch (error) {
